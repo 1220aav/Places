8d511b8f3c02ad3cb3133314a6e3c195f7894d21
Andrey <1220aav@gmail.com>
2020-03-10 20:30:54 +0000

    Edited cell height and made images rounded

--- Places/MainViewController.swift
+++ Places/MainViewController.swift
@@ -28,8 +28,16 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = places[indexPath.row]
         cell.imageView?.image = UIImage(named: places[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
+    }
+    
+    // MARK: - Table View Delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     /*
