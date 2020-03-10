7b25e32a2e2ea518c0a8ee8aa58086a0f38373ea
Andrey <1220aav@gmail.com>
2020-03-10 20:53:01 +0000

    Rebuild the cell to custom view with more content

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Andrey Andryukhin on 10.03.2020.
+//  Copyright © 2020 Andrey Andryukhin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var placeLocationLabel: UILabel!
+    @IBOutlet weak var placeTypeLabel: UILabel!
+    
+}
